76e10b8054b77caa6d5b46fe0d672afea807cc9e
cmarin001 <cmarin6051@gmail.com>
2024-09-24 11:15:03 -0500

    chore: move fieldset tokens from foundations to component

---
+++ src/Fieldset/Tokens/tokens.ts
@@ -0,0 +1,12 @@
+import { inube } from "@inubekit/foundations";
+
+const tokens = {
+  legend: {
+    color: inube.palette.neutral.N200,
+  },
+  border: {
+    color: inube.palette.neutral.N40,
+  },
+};
+
+export { tokens };
